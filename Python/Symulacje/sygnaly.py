d191e478c5ea7dab232b171212301634d33cff39
Marek <m-czekala@o2.pl>
2020-04-04 18:47:01 +0200

    Sygnaly wczytywane z pliku
    Zmiana obliczania widma

--- Python/Symulacje/sygnaly.py
+++ Python/Symulacje/sygnaly.py
@@ -4,29 +4,17 @@ Modul do generacji sygnalow
 
 import numpy as np
 
-def multisin(f,fazy = np.array([]),LICZBA_PROBEK = 500,OFFSET = 250):
-    f = np.asarray(f)
-    liczba_harmonicznych = f.shape[0]
-    t = np.arange(0,1, (1/LICZBA_PROBEK))
-    sygnal = 0
-    DAC_MAX = 4095
-    if fazy.shape[0] == 0 :
-        fazy = np.load("../../../Symulacje/fazy_optymalne.npy")
-    for i in range(liczba_harmonicznych) :
-        sygnal += np.sin( 2*np.pi*f[i]*t + fazy[i] )
-    
-    if np.min(sygnal) < 0 :
-        sygnal -= np.min(sygnal) # odejmujemy ujemna -> dodajemy
-    
-    sygnal /= sygnal.max()
-    sygnal *= (DAC_MAX-OFFSET) / DAC_MAX
-    return sygnal
+sinus = np.load('sygnaly/sinus_uint16.npy')
+multisin = np.load('sygnaly/multisin_probki_uint16.npy')
 
-def widmoMultisin(sygnal,f,Fs = 500):
+def widmo(sygnal,f,Fs = 500):
+    DAC_MAX = 4096
+    DAC_VREF = 1.0
+    sygnal = DAC_VREF * (sygnal / DAC_MAX)
     n = len(sygnal) # length of the signal
     k = np.arange(n)
     T = n/Fs
-    frq = k/T # two sides frequency range
+    frq = k * T # two sides frequency range
     frq = frq[range(int(n/2))] # one side frequency range
 
     Y = np.fft.fft(sygnal)/n # fft computing and normalization
@@ -34,7 +22,10 @@ def widmoMultisin(sygnal,f,Fs = 500):
 
     modul = abs(Y)
     szukane_widmo = []
+    f = f // f[0]
+    f = f.astype('uint16')
+    #print(f)
     for F in f :
         szukane_widmo.append(modul[F])
     szukane_widmo = np.asarray(szukane_widmo)       
-    return szukane_widmo
+    return  szukane_widmo
