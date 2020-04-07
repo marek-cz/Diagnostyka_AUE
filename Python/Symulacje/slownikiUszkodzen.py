e38f5a509def05e83db670b7efdc8469ae9203c3
Marek <m-czekala@o2.pl>
2020-04-07 15:28:09 +0200

    Skrypt zapisujacy slownik uszkodzen wskazanego ukladu do plikow
    W katalogu /slowniki_uszkodzen powstaje folder o nazwie ukladu np
    HPF_MFB, w ktorym w folderze Slownik_Multisin zapisane sa tablice numpy
    zawierajace slownik uszkodzen

---
+++ Python/Symulacje/slownikiUszkodzen.py
@@ -0,0 +1,33 @@
+import funkcjeUkladowe
+
+print("Symulacje w toku...\n")
+
+#def slownikUszkodzenMultisin():
+sygnal = funkcjeUkladowe.sygnaly.multisin
+slownikUszkodzenMultisin = funkcjeUkladowe.slownikUszkodzen(sygnal)
+
+
+
+# wydobycie nazwy badanego układu:
+nazwa_pliku_z_ukladem = funkcjeUkladowe.nazwa
+indeks_kropki = nazwa_pliku_z_ukladem.find('.')
+nazwa_ukladu = nazwa_pliku_z_ukladem[:indeks_kropki]
+
+# przejscie do katalogu slowniki uszkodzen :
+# po wyborze układu jestesmy w folderze uklady
+funkcjeUkladowe.os.chdir('../../slowniki_uszkodzen')
+if not(funkcjeUkladowe.os.path.exists(nazwa_ukladu)):
+    # jezeli folder nie istnieje tworzymy go
+    funkcjeUkladowe.os.mkdir(nazwa_ukladu)
+funkcjeUkladowe.os.chdir(nazwa_ukladu)
+if not(funkcjeUkladowe.os.path.exists('Slownik_Multisin')):
+    # jezeli folder nie istnieje tworzymy go
+    funkcjeUkladowe.os.mkdir('Slownik_Multisin')
+funkcjeUkladowe.os.chdir('Slownik_Multisin')
+# zapis slownika do plikow numpy:
+for sygnatura in slownikUszkodzenMultisin:
+    funkcjeUkladowe.np.save(sygnatura, slownikUszkodzenMultisin[sygnatura])
+
+
+print("Slownik uszkodzen w folderze : \n",funkcjeUkladowe.os.getcwd())
+
