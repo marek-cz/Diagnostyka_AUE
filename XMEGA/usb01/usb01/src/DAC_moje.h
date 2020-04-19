d7a49357da859c60f928f1298fd1e82bbc89adae
Marek <m-czekala@o2.pl>
2020-04-19 10:37:26 +0200

    Usuniecie z pliku naglowkowego nieuzywanych deklaracji

--- XMEGA/usb01/usb01/src/DAC_moje.h
+++ XMEGA/usb01/usb01/src/DAC_moje.h
@@ -10,27 +10,14 @@
 #define DAC_MOJE_H_
 
 #include <avr/pgmspace.h>
-#define OFFSET_TABLICY 0
 #define LICZBA_PROBEK_W_TABLICY_MAX 500
-//#define KALIB_CZEST 
 
 extern const uint16_t multi_sin_wave_500[LICZBA_PROBEK_W_TABLICY_MAX];	// probki sygnalu wieloharmonicznego
-extern const uint16_t multi_sin_wave_250[250];	// probki sygnalu wieloharmonicznego
-extern const uint16_t multi_sin_wave_100[100];	// probki sygnalu wieloharmonicznego
 extern const uint16_t sinc_wave_500[LICZBA_PROBEK_W_TABLICY_MAX];		// probki sygnalu sinc
-extern const uint16_t sinc_wave_250[250];		// probki sygnalu sinc
-extern const uint16_t sinc_wave_100[100];		// probki sygnalu sinc
 extern const uint16_t sinus_500[LICZBA_PROBEK_W_TABLICY_MAX];			// probki sygnalu sinusa
 extern const uint16_t sinus_250[250];			// probki sygnalu sinusa
 extern const uint16_t sinus_100[100];			// probki sygnalu sinusa
-//extern const uint16_t pila_1000[LICZBA_PROBEK_W_TABLICY_MAX];			// probki sygnalu pily (rampa)
 
-#ifdef KALIB_CZEST
-	extern const uint16_t sqr_2[2];
-	extern const uint16_t sqr_10[10];
-	extern const uint16_t sqr_100[100];
-	extern const uint16_t sqr_1000[1000];
-#endif
 
 
 /*		funkcje		*/
