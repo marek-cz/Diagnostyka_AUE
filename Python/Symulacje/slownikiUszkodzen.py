581c82ee01e21b40a43109492694a0c4e6ec5d04
Marek <m-czekala@o2.pl>
2020-04-19 14:46:11 +0200

    Symulacje dostarczaja wszystkich niezbednych danych dla multisin

--- Python/Symulacje/slownikiUszkodzen.py
+++ Python/Symulacje/slownikiUszkodzen.py
@@ -1,6 +1,7 @@
 import funkcjeUkladowe
 
-PROG = 0.99
+PROG = 0.9973 # 3 sigma
+DELTA_S_CONST = 5e-5
 
 #---------------------------------------------------------------
 def odlegloscMahalanobisa(x,y,C):
@@ -51,14 +52,45 @@ def wyznaczElipse(macierz_skalujaca, odleglosc_Mahalanobisa, srodek):
     
     return x,y
 
+def wyznaczMacierzSkalujaca(Klaster_danych):
+    liczba_pomiarow = Klaster_danych.shape[1] # liczba pomiarow
+    C = funkcjeUkladowe.np.cov( Klaster_danych ) # wyznaczanie macierzy kowariancji
+    C_inv = funkcjeUkladowe.np.linalg.inv(C) # odwrocenie macierzy kowariancji
+    wartosci_wlasne, wektory_wlasne = funkcjeUkladowe.np.linalg.eig( C_inv ) # wyznaczenie wektorow i wartosci wlasnych macierzy odwrotnej do macierzy kowariancji
 
+    lambda_min = wartosci_wlasne.min()
+    LAMBDA_diag = funkcjeUkladowe.np.diag( wartosci_wlasne / lambda_min ) # normowanie wartosci wlasnych
+    U = wektory_wlasne
+
+    C1 = funkcjeUkladowe.np.matmul( funkcjeUkladowe.np.matmul( U, LAMBDA_diag ), funkcjeUkladowe.np.transpose(U) ) # wyznaczenie unormowanej macierzy skalujacej
+
+    return C1
+
+def wyznaczGranicznaOdlegloscMahalanobisa(Klaster_danych, srodek, macierz_skalujaca ):
+    s_graniczna = 0.0
+    liczba_pomiarow = Klaster_danych.shape[1] # liczba pomiarow
+    delta_s = odlegloscMahalanobisa( Klaster_danych[:,0], srodek, macierz_skalujaca) # pierwszy pomiar - stan nominalny
+    if (delta_s == 0) : delta_s = DELTA_S_CONST # zabezpieczenie
+    # iteracyjne wyznaczanie odleglosci granicznej
+    while (True): 
+        licznik = 0
+        for i in range(liczba_pomiarow):
+                x = Klaster_danych[:,i]
+                d = odlegloscMahalanobisa(x, srodek, macierz_skalujaca)
+                if (d < s_graniczna) : licznik += 1
+        if licznik / liczba_pomiarow > PROG : break
+        s_graniczna += delta_s
+
+    return s_graniczna
 #---------------------------------------------------------------
 
 print("Symulacje w toku...\n")
 
 #def slownikUszkodzenMultisin():
 sygnal = funkcjeUkladowe.sygnaly.multisin
-slownikUszkodzenMultisin = funkcjeUkladowe.slownikUszkodzen(sygnal)
+f_multisin = funkcjeUkladowe.uklad.BADANE_CZESTOTLIWOSCI_MULTISIN
+f_sinc = funkcjeUkladowe.uklad.BADANE_CZESTOTLIWOSCI_SINC
+slownikUszkodzenMultisin = funkcjeUkladowe.slownikUszkodzen( f_multisin ,sygnal)
 
 
 
@@ -78,63 +110,99 @@ if not(funkcjeUkladowe.os.path.exists(nazwa_ukladu)):
     # jezeli folder nie istnieje tworzymy go
     funkcjeUkladowe.os.mkdir(nazwa_ukladu)
 funkcjeUkladowe.os.chdir(nazwa_ukladu)
-if not(funkcjeUkladowe.os.path.exists('Slownik_Multisin')):
+if not(funkcjeUkladowe.os.path.exists('Slowniki_Multisin')):
     # jezeli folder nie istnieje tworzymy go
-    funkcjeUkladowe.os.mkdir('Slownik_Multisin')
-funkcjeUkladowe.os.chdir('Slownik_Multisin')
-# zapis slownika do plikow numpy:
-for sygnatura in slownikUszkodzenMultisin:
-    funkcjeUkladowe.np.save(sygnatura, slownikUszkodzenMultisin[sygnatura])
+    funkcjeUkladowe.os.mkdir('Slowniki_Multisin')
+funkcjeUkladowe.os.chdir('Slowniki_Multisin')
 
-ObszarTolerancji = funkcjeUkladowe.sygnaly.widmo(sygnal, funkcjeUkladowe.uklad.BADANE_CZESTOTLIWOSCI) * funkcjeUkladowe.monteCarloNormal(liczba_losowanMC = funkcjeUkladowe.uklad.LICZBA_LOSOWAN_PUNKT_NOMINALNY)
-wartosc_srednia = ObszarTolerancji.mean(axis=0) # wektor!
-sigma = ObszarTolerancji.std(axis=0)            # wektor!
-
-#zapsianie wartosci sredniej i odchylenia std do plikow
-funkcjeUkladowe.np.save("../wartosc_srednia", wartosc_srednia)
-funkcjeUkladowe.np.save("../odchylenie_std", sigma)
+funkcjeUkladowe.np.save('f_multisin',f_multisin)
+ObszarTolerancji = funkcjeUkladowe.sygnaly.widmo(sygnal, f_multisin) * funkcjeUkladowe.monteCarloNormal( f_multisin ,liczba_losowanMC = funkcjeUkladowe.uklad.LICZBA_LOSOWAN_PUNKT_NOMINALNY)
 
 
-decyzja = input("Czy symulowac macierz danych? T/N ")
+#decyzja = input("Czy symulowac macierz danych? T/N ")
+#if decyzja == 'T' :
+#    X = funkcjeUkladowe.wygenerujMacierzDanychPCA(f_multisin, sygnal, funkcjeUkladowe.uklad.LICZBA_LOSOWAN_MC)
+#    funkcjeUkladowe.np.save("macierz_danych_multisin_"+nazwa_ukladu, X)
 
-if decyzja == 'T' :
-    X = funkcjeUkladowe.wygenerujMacierzDanychPCA(sygnal , funkcjeUkladowe.uklad.LICZBA_LOSOWAN_MC)
-    funkcjeUkladowe.np.save("../macierz_danych_"+nazwa_ukladu, X)
-
-X = funkcjeUkladowe.np.load("../macierz_danych_"+nazwa_ukladu+".npy")
+X = funkcjeUkladowe.np.load("macierz_danych_multisin_"+nazwa_ukladu+".npy")
 
 PCA_FI_2_SKLADOWE = funkcjeUkladowe.PCA(X, 2)
 PCA_FI_3_SKLADOWE = funkcjeUkladowe.PCA(X, 3)
 
-funkcjeUkladowe.np.save('../PCA_2_SKL', PCA_FI_2_SKLADOWE)
-funkcjeUkladowe.np.save('../PCA_3_SKL', PCA_FI_3_SKLADOWE)
+funkcjeUkladowe.np.save('PCA_2_SKL', PCA_FI_2_SKLADOWE)
+funkcjeUkladowe.np.save('PCA_3_SKL', PCA_FI_3_SKLADOWE)
 
-# wyznaczenie granicznej odległości Mahalanobisa dla PCA - 2 składowe główne
+###################################################################################################
 
+#  PCA - 2 składowe główne
+
+###################################################################################################
 Klaster_danych = funkcjeUkladowe.np.matmul( PCA_FI_2_SKLADOWE, funkcjeUkladowe.np.transpose( ObszarTolerancji ) ) # transpozycja, bo w PCA mamy miec pomiary w kolumnach, a w ObszatTolerancji sa w wierszach
-liczba_pomiarow = Klaster_danych.shape[1] # liczba pomiarow
-C = funkcjeUkladowe.np.cov( Klaster_danych ) # wyznaczanie macierzy kowariancji
-C_inv = funkcjeUkladowe.np.linalg.inv(C) # odwrocenie macierzy kowariancji
-wartosci_wlasne, wektory_wlasne = funkcjeUkladowe.np.linalg.eig( C_inv ) # wyznaczenie wektorow i wartosci wlasnych macierzy odwrotnej do macierzy kowariancji
 
-lambda_min = wartosci_wlasne.min()
-LAMBDA_diag = funkcjeUkladowe.np.diag( wartosci_wlasne / lambda_min ) # normowanie wartosci wlasnych
-U = wektory_wlasne
+C1 = wyznaczMacierzSkalujaca(Klaster_danych) # macierz skalujaca do odleglosci Mahalanobisa
+srodek = Klaster_danych.mean(axis=1)# wektor wartosci sredniej 
+s_graniczna_PCA_2 = wyznaczGranicznaOdlegloscMahalanobisa(Klaster_danych,srodek,C1) # graniczna odleglosc Mahalanobisa - jezeli punkt jets dalej, jest poza obszarem nominalnym
 
-C1 = funkcjeUkladowe.np.matmul( funkcjeUkladowe.np.matmul( U, LAMBDA_diag ), funkcjeUkladowe.np.transpose(U) ) # wyznaczenie unormowanej macierzy skalujacej
+slownik_uszkodzen_PCA_2 = funkcjeUkladowe.slownikUszkodzenPCA(slownikUszkodzenMultisin, PCA_FI_2_SKLADOWE)
 
-srodek = funkcjeUkladowe.np.matmul( PCA_FI_2_SKLADOWE, wartosc_srednia )
-s_graniczna = 0.0
-delta_s = odlegloscMahalanobisa( Klaster_danych[:,0], srodek, C1) # pierwszy pomiar - stan nominalny
-# iteracyjne wyznaczanie odleglosci granicznej
-while (True): 
-    licznik = 0
-    for i in range(liczba_pomiarow):
-	    x = Klaster_danych[:,i]
-	    d = odlegloscMahalanobisa(x,srodek,C1)
-	    if (d < s_graniczna) : licznik += 1
-    if licznik / liczba_pomiarow > PROG : break
-    s_graniczna += delta_s
+funkcjeUkladowe.np.save('srodek_PCA_2_skladowe', srodek)
+funkcjeUkladowe.np.save('macierz_skalujaca_PCA_2_skladowe', C1)
+funkcjeUkladowe.np.save('graniczna_odleglosc_PCA_2_skladowe', s_graniczna_PCA_2)
+
+if not(funkcjeUkladowe.os.path.exists('Slownik_PCA_2')):
+    # jezeli folder nie istnieje tworzymy go
+    funkcjeUkladowe.os.mkdir('Slownik_PCA_2')
+funkcjeUkladowe.os.chdir('Slownik_PCA_2')
+# zapis slownika do plikow numpy:
+for sygnatura in slownik_uszkodzen_PCA_2:
+    funkcjeUkladowe.np.save(sygnatura, slownik_uszkodzen_PCA_2[sygnatura])
 
 
-print("Slownik uszkodzen w folderze : \n",funkcjeUkladowe.os.getcwd())
+print("Analiza PCA dla dwoch skladowych glownych zostala zakonczona")
+print("Srodek: ", srodek)
+print("Macierz skalujaca: \n", C1)
+print("\nOdleglsoc graniczna : ", s_graniczna_PCA_2)
+#decyzja = input("Czy wyrysowac krzywe identyfikacyjne dla dwoch skladowych glownych? T/N ")
+#if decyzja == 'T' : funkcjeUkladowe.wyrysujKrzyweIdentyfikacyjne2D(slownik_uszkodzen_PCA_2)
+############################################
+#   EWENTUALNE LACZENIE SYGNATUR
+###########################################
+
+funkcjeUkladowe.os.chdir('../')  # <- powrot do katalogu Slowniki_Multisin
+###################################################################################################
+
+#  PCA - 3 składowe główne
+
+###################################################################################################
+Klaster_danych = funkcjeUkladowe.np.matmul( PCA_FI_3_SKLADOWE, funkcjeUkladowe.np.transpose( ObszarTolerancji ) ) # transpozycja, bo w PCA mamy miec pomiary w kolumnach, a w ObszatTolerancji sa w wierszach
+
+C1 = wyznaczMacierzSkalujaca(Klaster_danych) # macierz skalujaca do odleglosci Mahalanobisa
+srodek = Klaster_danych.mean(axis=1)# wektor wartosci sredniej 
+s_graniczna_PCA_3 = wyznaczGranicznaOdlegloscMahalanobisa(Klaster_danych,srodek,C1) # graniczna odleglosc Mahalanobisa - jezeli punkt jets dalej, jest poza obszarem nominalnym
+
+slownik_uszkodzen_PCA_3 = funkcjeUkladowe.slownikUszkodzenPCA(slownikUszkodzenMultisin, PCA_FI_3_SKLADOWE)
+
+funkcjeUkladowe.np.save('srodek_PCA_3_skladowe', srodek)
+funkcjeUkladowe.np.save('macierz_skalujaca_PCA_3_skladowe', C1)
+funkcjeUkladowe.np.save('graniczna_odleglosc_PCA_3_skladowe', s_graniczna_PCA_3)
+
+if not(funkcjeUkladowe.os.path.exists('Slownik_PCA_3')):
+    # jezeli folder nie istnieje tworzymy go
+    funkcjeUkladowe.os.mkdir('Slownik_PCA_3')
+funkcjeUkladowe.os.chdir('Slownik_PCA_3')
+# zapis slownika do plikow numpy:
+for sygnatura in slownik_uszkodzen_PCA_3:
+    funkcjeUkladowe.np.save(sygnatura, slownik_uszkodzen_PCA_3[sygnatura])
+
+
+print("Analiza PCA dla trzech skladowych glownych zostala zakonczona")
+print("Srodek: ", srodek)
+print("Macierz skalujaca: \n", C1)
+print("\nOdleglsoc graniczna : ", s_graniczna_PCA_3)
+#decyzja = input("Czy wyrysowac krzywe identyfikacyjne dla trzech skladowych glownych? T/N ")
+#if decyzja == 'T' : funkcjeUkladowe.wyrysujKrzyweIdentyfikacyjne3D(slownik_uszkodzen_PCA_3)
+
+#----------------------------------------------------------------------------------------------------
+funkcjeUkladowe.os.chdir('../')  # <- powrot do katalogu Slowniki_Multisin
+print("Symulacje zakonczone")
+print("Slowniki uszkodzen w folderze : \n",funkcjeUkladowe.os.getcwd())
