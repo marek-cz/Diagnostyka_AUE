cf744caa7eb7595bc0378ae1d9343527f79af16e
Marek <m-czekala@o2.pl>
2020-04-30 10:56:12 +0200

    dodanie symulacji widma dla sygnału sinc
    - w pliku mamy zapisane probki sygnalu -sinc(2*10*(t-5))
    - funckja liczaca TF przyjmuje 2 arumenty:
    	- sygnal
    	- frq
    - frq - wektor czestotliwosci pomiarowych, ustawionych ROSNACO!
    - ustalamy czas akwizycji T=(1/f_min)=(1/frq[0])
    - ustalamy wektor czasow tn- od 0 do T z krokiem T_s = (T/N)
    - usuwamy z sygnalu skladowa stala, ktora jest interpretowana
      przez TF jako impuls prostokątny i psuje widmo!
    - liczymy TF, zwracamy modul

--- Python/Symulacje/sygnaly.py
+++ Python/Symulacje/sygnaly.py
@@ -6,6 +6,7 @@ import numpy as np
 
 sinus = np.load('sygnaly/sinus_uint16.npy')
 multisin = np.load('sygnaly/multisin_probki_uint16.npy')
+sinc = np.load('sygnaly/sinc_uint16.npy')
 
 def widmo(sygnal,f,Fs = 500):
     DAC_MAX = 4096
@@ -29,3 +30,37 @@ def widmo(sygnal,f,Fs = 500):
         szukane_widmo.append(modul[F])
     szukane_widmo = np.asarray(szukane_widmo)       
     return  szukane_widmo
+
+
+def TransformataFouriera(sygnal, frq):
+    """
+    Funckja wyznaczająca transformate Fouriera dla liniowej aproksymacji sygnalu
+    na częstotliwości (częstotliwościach) zawartych w frq - wektor w ktorym rosnaco
+    sa zapsiane czestotliwosci od f_min do f_max
+    """
+    i = 0
+    ReU = np.zeros(len(frq))
+    ImU = np.zeros(len(frq))
+    T_ack = 1/frq[0]    # czas akwizycji
+    N = len(sygnal)     # liczba probek
+
+    tn = np.arange(0, T_ack, (T_ack / N) )    # wektor czasow
+    un = sygnal.astype('float32')     # dla uproszczenia zapisu
+    un -= un[0]         # usuniecie skladowej stalej interpretowanej przez TF jako impuls prostokatny
+    un = un/4095
+    for f in frq:
+        w = 2 * np.pi * f
+        sin_w_tn = np.sin(w*tn)
+        cos_w_tn = np.cos(w*tn)
+        du_dt = ( ( un[1:] - un[:-1] ) / ( tn[1:] - tn[:-1] ) )
+        
+        ReU[i] = sum( ( 1/w)*( un[1:] * sin_w_tn[1:] - un[:-1] * sin_w_tn[:-1] ) + du_dt * ((cos_w_tn[1:] - cos_w_tn[:-1]) / (w*w) ) )
+        ImU[i] = sum( (-1/w)*( un[1:] * cos_w_tn[1:] - un[:-1] * cos_w_tn[:-1] ) + du_dt * ((sin_w_tn[1:] - sin_w_tn[:-1]) / (w*w) ) )
+        
+        i = i + 1
+    
+    modul = np.sqrt( ReU*ReU + ImU * ImU )
+    #return (ReU,ImU)
+    return modul
+
+    
