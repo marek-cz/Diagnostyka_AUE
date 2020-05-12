24431b5727c9f7f7ec53e8ce321dd5a0c7c63424
Marek <m-czekala@o2.pl>
2020-05-12 17:26:59 +0200

    Plik definiujacy polaczenia MCU z LCD -> na 256A3BU!!!

---
+++ XMEGA/usb01/usb01/src/defines.h
@@ -0,0 +1,23 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <joerg@FreeBSD.ORG> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.        Joerg Wunsch
+ * ----------------------------------------------------------------------------
+ *
+ * General stdiodemo defines
+ *
+ * $Id: defines.h,v 1.2.2.1 2009/06/25 20:21:15 joerg_wunsch Exp $
+ */
+
+/* HD44780 LCD port connections */
+#define HD44780_RS C, 6
+#define HD44780_RW E, 5
+#define HD44780_E  C, 7
+/* The data bits have to be in ascending order. */
+#define HD44780_D4 C, 0
+
+/* Whether to read the busy flag, or fall back to
+   worst-time delays. */
+#define USE_BUSY_BIT 0
