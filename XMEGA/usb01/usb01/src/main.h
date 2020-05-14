13d131c6aaadd0494011dd2630a7bce86a6e7f37
Marek <m-czekala@o2.pl>
2020-05-14 09:51:34 +0200

    Makra do obslugi przyciskow i LEDow

--- XMEGA/usb01/usb01/src/main.h
+++ XMEGA/usb01/usb01/src/main.h
@@ -16,7 +16,7 @@
 #include <avr/io.h>
 //#include <stddef.h>
 #include <avr/interrupt.h>
-#include <util/delay.h>
+//#include <util/delay.h>
 #include <math.h>
 #include <stdbool.h>
 //----------------------------------------------------
@@ -33,11 +33,31 @@
 //----------------------------------------------------
 //				MAKRA
 #define ROZMIAR_RAMKI_USB_MAX		13
-#define ROZMIAR_KOMENDY_MAX			20
 #define KONIEC_ZBIERANIA_PROBEK (1<<0)
-#define ODBIOR_KOMENDY			(1<<1)
+//#define ODBIOR_KOMENDY			(1<<1)
 #define ODBIOR_ZNAKU			(1<<2)
+#define WYSLIJ_POMIAR			(1<<5)
 #define OKRES_TIMERA_MIN			31
+
+#define MULTISIN_DIAG_PER	3199
+#define SINC_DIAG_PER		1279
+#define DELAY_BEZ_ZMIANY_PRZEBIEGU_ms	100
+#define DELAY_ZE_ZMIANA_PRZEBIEGU_ms	500
+
+//				MAKRA PRZYCISKOW
+#define BUTTON_PORT		PORTC 
+#define BUTTON_1_PIN	0
+#define BUTTON_2_PIN	1
+//#define BUTTON_1_ISR_VEC PORTF_INT0_vect
+//#define BUTTON_2_ISR_VEC PORTF_INT1_vect
+//#define WCISNIECIE_BUTT_1		(1<<3)
+//#define WCISNIECIE_BUTT_2		(1<<4)
+
+#define  DEBOUNCING_DELAY_ms	50
+//			MAKRA LEDow:
+#define LED_PORT		PORTC
+#define LED_NOM_PIN		4
+#define LED_FAULT_PIN	5
 
 //			MAKRA OPISUJACE NUMERY PRZEBIEGOW
 #define SINUS_500_NR			0
@@ -93,6 +113,8 @@
 //----------------------------------------------------
 //				FUNKCJA INICJALIZACJI
 void Init(void);
+void ButtonInit(PORT_t *port);
+void LEDInit(PORT_t *port);
 //----------------------------------------------------
 //				FUNKCJE TAKTOWANIA
 bool OSC_wait_for_rdy(uint8_t clk);
@@ -104,6 +126,7 @@ void PomiarOkresowyADC(uint16_t liczba_probek, volatile uint16_t opoznienie);			
 void WyborPrzebiegu(uint8_t przebieg, uint16_t liczba_probek);
 void PomiarImpulsowy(uint16_t liczba_probek, volatile uint16_t opoznienie);						// wykorzystanie SINC
 void delayTCC1(uint16_t ms);
+//void debouncing(volatile uint8_t *rejestr_portu, uint8_t pin, uint8_t opoznienie_ms );
 //----------------------------------------------------
 //				FUNKCJE KONWERSJI
 void NadajWynik(uint16_t * tablicaProbek, uint16_t liczbaProbek);
@@ -116,7 +139,8 @@ uint16_t znakiNaLiczbe( unsigned char tablica_znakow[] ,uint8_t start_ind);
 uint8_t ReadCalibrationByte(uint8_t index); // kalibracja ADC
 void WlaczPeryferia(void);
 float oblicz_DFT(uint16_t k , uint16_t N, const uint16_t sygnal[] );
-float obliczTF(const uint16_t sygnal[],uint16_t liczba_elementow,uint8_t f, uint16_t okres_timera);
+float obliczTF(const uint16_t sygnal[],uint16_t liczba_elementow,uint16_t f, uint16_t okres_timera);
+void wyznaczWimo(float wynik[], uint16_t liczba_elementow, uint16_t okres_timera , uint8_t typ_slownika) ;
 void analizaRamkiDanych(uint16_t * okres_timera,uint16_t * liczba_probek,uint8_t * przebieg, unsigned char ramka_danych[]);
 uint16_t KalibracjaOffsetuADC(void);
 float KalibracjaWzmocnieniaADC(void);
@@ -125,5 +149,7 @@ float KalibracjaWzmocnieniaADC(void);
 //				FUNKCJE DIAGNOSTYCZNE
 uint8_t Diagnostyka(float * widmo, uint8_t typ_slownika);
 void wypiszWynikDiagnozyLCD(uint8_t etykieta_uszkodzenia);
+uint8_t SprawdzParametryPrzebiegu(uint16_t okres_timerow,uint8_t przebieg, uint16_t liczba_probek, uint8_t przebieg_docelowy);
+void Testuj(uint16_t *okres_timerow,uint8_t *przebieg, uint16_t * liczba_probek, uint8_t przebieg_docelowy, uint8_t typ_slownika);
 
 #endif /* MAIN_H_ */
