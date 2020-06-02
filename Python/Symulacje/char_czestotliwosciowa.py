a2ddce6dbbb693800f7033a4453dabc16b4ce8af
Marek <m-czekala@o2.pl>
2020-06-02 13:55:53 +0200

    Poprawa opisu wykresu + wyznaczanie wariancji z Monte Carlo
    - wariancja -> do pracy

--- Python/Symulacje/char_czestotliwosciowa.py
+++ Python/Symulacje/char_czestotliwosciowa.py
@@ -21,7 +21,10 @@ def wyrysujCharakterystyki(elementy,f):
     minimum = 10000* np.ones(f.shape[0])
     maksimum = np.zeros(f.shape[0])
     plt.clf()
-    for i in range (10):
+    (licznik, mianownik) = uklad.transmitancja(uklad.elementy)
+    h_nom = funkcjeUkladowe.charCzestotliwosciowaModul(licznik, mianownik,f)
+    H = np.asarray(h_nom)
+    for i in range (200):
         for element in elementy :
             if element.find('R') != -1 :
                 #element jest Rezystorem
@@ -37,31 +40,46 @@ def wyrysujCharakterystyki(elementy,f):
             h = funkcjeUkladowe.charCzestotliwosciowaModul(licznik, mianownik,f)
             #if h[150] > maksimum[150] : maksimum = h
             #if h[150] < minimum[150] : minimum = h
-            plt.plot(f, 20 * np.log10(abs(h)),'r')
+            plt.plot(f, 20 * np.log10(abs(h)),'r',linewidth=4)
+            H = np.concatenate( (H, h) )
     #plt.plot(f, 20 * np.log10(abs(maksimum)),'r')
     #print(f,len(f))
-    (licznik, mianownik) = uklad.transmitancja(uklad.elementy)
-    h = funkcjeUkladowe.charCzestotliwosciowaModul(licznik, mianownik,f)
-    plt.plot(f, 20 * np.log10(abs(h)),'b',label = 'Nominalna')
+    plt.plot(f, 20 * np.log10(abs(h)),'r', label = 'Losowania' ,linewidth=4)
+    plt.plot(f, 20 * np.log10(abs(h_nom)),'b',label = 'Nominalna',linewidth=4)
     plt.semilogx()
-    plt.title('Charakterystyka amplitudowa filtru')
-    plt.xlabel('Częstotliwość [Hz]')
-    plt.ylabel('|H| [dB]')
-    plt.margins(0, 0.1)
-    plt.grid(which='both', axis='both')
-    plt.legend()
+    plt.title('Charakterystyka amplitudowa filtru', fontsize=26)
+    plt.xlabel("Częstotliwość [Hz]", fontsize=30)
+    plt.ylabel('|H(f)| [dB]', fontsize=30)
+    plt.axis('equal')
+    plt.grid()
+    plt.legend(prop={'size': 30})
     plt.show()
+    plt.show()
+
+    return H
     
 #------------------------------------------------------------------------------
 # 2. TRANSMITANCJA 
 (licznik, mianownik) = uklad.transmitancja(uklad.elementy)
 
 
-plt.rc('axes', labelsize = 14)
-plt.rc('xtick', labelsize=12)
-plt.rc('ytick', labelsize=12) 
+##plt.rc('axes', labelsize = 14)
+##plt.rc('xtick', labelsize=12)
+##plt.rc('ytick', labelsize=12) 
 
-f = np.arange(1,5000,1)
+f = np.logspace(1,3,1000)
 
 # na podstawie dokumentacji scipy
-wyrysujCharakterystyki(uklad.elementy,f)
+H = wyrysujCharakterystyki(uklad.elementy,f)
+
+H = H.reshape ( ( H.shape[0] // f.shape[0], f.shape[0] ) )
+var = H.var(axis = 0)
+
+plt.semilogx(f, var,'b',linewidth=4)
+plt.title('Wariancja charakterystyki amplitudowej', fontsize=26)
+plt.xlabel("Częstotliwość [Hz]", fontsize=30)
+plt.ylabel('Wariancja |H(f)| [V/V]', fontsize=30)
+plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+plt.grid()
+plt.show()
+plt.show()
