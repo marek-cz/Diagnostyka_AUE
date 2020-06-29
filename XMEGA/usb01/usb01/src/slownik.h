98a5794947f1339e20b74233b7e9b39b803a52d9
Marek <m-czekala@o2.pl>
2020-06-29 09:54:41 +0200

    Dodanie makra sterujacego wyborem układu
    Poprzez zakomentowanie/ odkomentowanie makra wybieramy
    ktory uklad ma byc diagnozowany przez MCU

--- XMEGA/usb01/usb01/src/slownik.h
+++ XMEGA/usb01/usb01/src/slownik.h
@@ -19,6 +19,13 @@
 #define SLOWNIK_SINC		0
 #define SLOWNIK_MULTISIN	1
 
+//	UKLAD:
+//#define HPF
+
+#ifndef HPF
+	#define BPF
+#endif
+
 //	MAKRA OPISUJACE WYMIARY
 #define WYMIAR_PO_PCA 2
 #define LICZBA_PROBEK_WIDMA 10
@@ -27,6 +34,7 @@
 #define LICZBA_USZKODZEN    10
 #define LICZBA_ZNAKOW_SYGNATURY 4
 //	MAKRA OPISUJACE KOLEJNOSC USZKODZEN W SLOWNIKU:
+#ifdef HPF
 #define C1_PLUS     0
 #define C1_MINUS    1
 #define C2_PLUS     2
@@ -37,6 +45,20 @@
 #define R1_MINUS    7
 #define R2_PLUS     8
 #define R2_MINUS    9
+#endif
+
+#ifdef BPF
+#define C1_PLUS     0
+#define C1_MINUS    1
+#define C2_PLUS     2
+#define C2_MINUS    3
+#define R1_PLUS     4
+#define R1_MINUS    5
+#define R2_PLUS     6
+#define R2_MINUS    7
+#define R3_PLUS     8
+#define R3_MINUS    9
+#endif
 
 #define NOMINALNY	10
 //############################
